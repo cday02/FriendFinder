dee3107dfc6eb213a3e8a06f1a8966ae672a6a0d
FriscoDY <cday02@gmail.com>
2019-05-14 21:12:27 -0500

    fix server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlroutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on " + PORT);
